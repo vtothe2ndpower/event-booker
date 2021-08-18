d174dbc35e90f416f0d846da40e75b89cd443a09
Victor Vargas <vvargasnunez@gmail.com>
2021-08-18 15:36:30 -0700

    Add bookings routes

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -19,6 +19,19 @@ const events = async eventIds => {
     }
 }
 
+const singleEvent = async eventId => {
+    try {
+        const event = await Event.findById(eventId);
+        return { 
+            ...event._doc, 
+            _id: event.id, 
+            creator: user.bind(this, event.creator) 
+        }
+    } catch(err) {
+        throw err;
+    }
+}
+
 const user = async userId => {
     try {
         const user = await User.findById(userId);
@@ -30,7 +43,7 @@ const user = async userId => {
     } catch (err) {
         throw err;
     }
-};
+}
 
 module.exports = {
     events: async () => {
@@ -55,6 +68,8 @@ module.exports = {
                 return {
                     ...booking._doc,
                     _id: booking.id,
+                    user: user.bind(this, booking._doc.user),
+                    event: singleEvent.bind(this, booking._doc.event),
                     createdAt: new Date(booking._doc.createdAt).toISOString(),
                     updatedAt: new Date(booking._doc.updatedAt).toISOString()
                 };
@@ -121,13 +136,29 @@ module.exports = {
                 user: '610f0570385b2a49abda2108',
                 event: fetchedEvent
             });
-            const result = await booking.save();
+            const result = await booking.save(); 
             return { 
                 ...result._doc, 
                 _id: result.id,
+                user: user.bind(this, booking._doc.user),
+                event: singleEvent.bind(this, booking._doc.event),
                 createdAt: new Date(result._doc.createdAt).toISOString(),
                 updatedAt: new Date(result._doc.updatedAt).toISOString() 
             };
+        } catch (err) {
+            throw err;
+        }
+    },
+    cancelBooking: async args => {
+        try {
+            const booking = await Booking.findById(args.bookingId).populate('event');
+            const event = { 
+                ...booking.event._doc, 
+                _id: booking.event.id, 
+                creator: user.bind(this, booking.event._doc.creator) 
+            };
+            await Booking.deleteOne({ _id: args.bookingId });
+            return event;
         } catch (err) {
             throw err;
         }
